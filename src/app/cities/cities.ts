52624ed9c34e50aea9f3b89b3e3378db351e55a5
Jezzzae <Jessicavences65@gmail.com>
2022-01-21 14:04:48 -0600

    cities.ts file

---
+++ src/app/cities/cities.ts
@@ -0,0 +1,23 @@
+export const CITIES = [
+    {
+        id: 1,
+        name: 'San Fransisco',
+        description: 'city of stars'
+    },
+    {
+        id: 2,
+        title: 'London',
+        description: 'lets go ted lasso'
+    },
+    {
+        id: 3,
+        title: 'Seattle',
+        description: 'Visit Forks!'
+    },
+    {
+        id: 4,
+        title: 'Sydney',
+        description: 'nemo'
+    },
+   
+]
