56a5a70aa651cdde1fa5ff92a0c7052df536bcc4
Jezzzae <Jessicavences65@gmail.com>
2022-01-21 10:52:58 -0600

    added components for cities and posts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { SiteHeaderComponent } from './site-header/site-header.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { TopicComponent } from './topic/topic.component';
+import { CitiesComponent } from './cities/cities.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SiteHeaderComponent,
     GalleryComponent,
-    TopicComponent
+    TopicComponent,
+    CitiesComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule
