41e7eda7436a1c1780253422f615a713f4e560f6
Ruchi Agarwal <ruchismailbox@gmail.com>
2022-01-24 14:39:41 -0600

    Added search to header

--- src/app/site-header/site-header.component.ts
+++ src/app/site-header/site-header.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-site-header',
   templateUrl: './site-header.component.html',
-  styleUrls: ['./site-header.component.css']
+  styleUrls: ['./site-header.component.css'],
 })
 export class SiteHeaderComponent implements OnInit {
+  searchterm: string = '';
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  action() {
+    this.router.navigate(['/posts/' + this.searchterm]);
   }
-
 }
